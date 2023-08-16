3f1836676fbe226fc7efcc402f69696d4079e937
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-08-16 07:46:55 +0300

    Update add_0.py

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def add(a, b):
-    #res = a + b
-    return a + b
+    """Addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
