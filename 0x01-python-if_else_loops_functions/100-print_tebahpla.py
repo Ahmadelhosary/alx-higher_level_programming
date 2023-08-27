8cfceec486e589a79b0789d3c524a09e66362f2c
Ahmad Mahmoud Mohamad <55003947+Ahmadelhosary@users.noreply.github.com>
2023-08-27 17:31:22 +0300

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""program that prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase"""
 
 for code in range(122, 96, -1):
     print("{}{}".format(chr(code), chr(code - 32)), end="")
