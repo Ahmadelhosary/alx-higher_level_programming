d1487abe29608ecc1575aba796fd23065921f61a
Ahmadelhosary <Ahmadelhosary>
2023-09-06 01:01:18 +0300

    added some files

--- 0x07-python-test_driven_development/tests/2-matrix_divided.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def matrix_divided(matrix, div):
-    result = []
-
-    # Divide each element in the matrix by the divisor
-    for row in matrix:
-        new_row = [element / div for element in row]
-        result.append(new_row)
